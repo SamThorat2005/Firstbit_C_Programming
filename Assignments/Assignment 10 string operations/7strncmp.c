b4d4388313ba97dc2c21ba9eadb2eb43f006740b
Samruddhi Thorat <samruddhist2005@gmail.com>
2026-03-15 19:15:21 +0530

    Delete Assignments/Assignment 10 string operations directory

--- Assignments/Assignment 10 string operations/7strncmp.c
+++
@@ -1,9 +0,0 @@
-//compares first n charcters 
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-    char str[]="hello";
-    char str1[]="hei";
-    printf("%d",strncmp(str,str1,2));
-}
--- Assignments/Assignment 10 string operations/8strchr.c
+++
@@ -1,8 +0,0 @@
-//strchr() , it is used to find first occurence of a character
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-    char str[]="hello";
-    printf("%s",strchr(str,'l'));
-}
